0fcbd8988eea98c3efb779b45397c322ddebf5ca
Thore Weilbier <thore@weilbier.net>
2019-06-12 13:48:59 +0200

    Improve report tool argument validation.

--- report-validator/report_validator/validation.py
+++ report-validator/report_validator/validation.py
@@ -1,6 +1,6 @@
 import click
 from deploy_tools.files import validate_and_format_address, InvalidAddressException
-from eth_utils import is_hex, is_0x_prefixed, to_bytes
+from eth_utils import is_hex, is_0x_prefixed, decode_hex
 
 
 def validate_address(ctx, param, value):
@@ -14,8 +14,7 @@ def validate_address(ctx, param, value):
 
 def validate_signature(ctx, param, value):
     if is_hex(value) and is_0x_prefixed(value) and len(value) == 132:
-        return to_bytes(hexstr=value)
-
+        return decode_hex(value)
     else:
         raise click.BadParameter(
             f"A signature parameter seems to be not well structured: {value}"
@@ -24,8 +23,7 @@ def validate_signature(ctx, param, value):
 
 def validate_block_header(ctx, param, value):
     if is_hex(value) and is_0x_prefixed(value):
-        return to_bytes(hexstr=value)
-
+        return decode_hex(value)
     else:
         raise click.BadParameter(
             f"A block header parameter seems to be not well structured: {value}"
@@ -33,31 +31,22 @@ def validate_block_header(ctx, param, value):
 
 
 def validate_equivocation_report_file(ctx, param, value):
-    try:
-        with open(value) as file:
-            lines = file.read().splitlines()
+    with open(value) as file:
+        lines = file.read().splitlines()
 
-        assert (
-            lines[0].startswith("Proposer:")
-            and lines[1].startswith("Block step:")
-            and lines[2].startswith("Detection time:")
-        ), "Report file header does not fit!"
+    if not (
+        lines[0].startswith("Proposer:")
+        and lines[1].startswith("Block step:")
+        and lines[2].startswith("Detection time:")
+    ):
+        raise click.BadParameter("Report file header does not fit!")
 
-        assert len(lines) >= 21, "Report file not long enough!"
+    if len(lines) < 21:
+        raise click.BadParameter("Report file not long enough!")
 
-        # Should be enough to attempt for parsing the necessary values.
-
-        return {
-            "unsigned_block_header_one": validate_block_header(None, None, lines[11]),
-            "signature_one": validate_signature(None, None, lines[14]),
-            "unsigned_block_header_two": validate_block_header(None, None, lines[17]),
-            "signature_two": validate_signature(None, None, lines[20]),
-        }
-
-    except AssertionError as e:
-        raise click.BadParameter(
-            f"The equivocation report file seems to be not well structured: {e}"
-        )
-
-    except click.BadParameter as e:
-        raise e
+    return {
+        "unsigned_block_header_one": validate_block_header(None, None, lines[11]),
+        "signature_one": validate_signature(None, None, lines[14]),
+        "unsigned_block_header_two": validate_block_header(None, None, lines[17]),
+        "signature_two": validate_signature(None, None, lines[20]),
+    }
--- report-validator/setup.py
+++ report-validator/setup.py
@@ -3,7 +3,6 @@ from setuptools import setup, find_packages
 setup(
     nam="report-validator",
     setup_requires="setuptools_scm",
-    # use_scm_version=True,
     version="0.0.1",
     packages=find_packages(),
     package_data={"report_validator": ["contracts.json"]},
