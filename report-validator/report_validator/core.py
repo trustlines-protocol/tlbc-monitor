c66f8eb564eea446082369cfa6fa48d477a26914
Thore Weilbier <thore@weilbier.net>
2019-06-12 14:26:18 +0200

    Simplify report tool for contract interaction
    To keep the tool small and better to maintain, the whole setup to get the validator set contract ABI has been simplified. Therefore a fixed and shorten ABI as dictionary has been placed within the core module. In favor all related parts to compile and retrieve the contract including the submodule of the blockchain, have been removed.
    
    Related to: #57
    See also: #65

--- report-validator/report_validator/core.py
+++ report-validator/report_validator/core.py
@@ -1,8 +1,23 @@
-import json
-import pkg_resources
-
 from deploy_tools.deploy import send_function_call_transaction
 from eth_utils import encode_hex
+
+
+VALIDATOR_SET_ABI_SIMPLIFIED = [
+    {
+        "constant": False,
+        "inputs": [
+            {"name": "_rlpUnsignedHeaderOne", "type": "bytes"},
+            {"name": "_signatureOne", "type": "bytes"},
+            {"name": "_rlpUnsignedHeaderTwo", "type": "bytes"},
+            {"name": "_signatureTwo", "type": "bytes"},
+        ],
+        "name": "reportMaliciousValidator",
+        "outputs": [],
+        "payable": False,
+        "stateMutability": "nonpayable",
+        "type": "function",
+    }
+]
 
 
 def report_malicious_validator(
@@ -16,7 +31,7 @@ def report_malicious_validator(
     signature_two,
 ):
     validator_set_contract = web3.eth.contract(
-        abi=get_contract_abi("ValidatorSet"), address=validator_set_contract_address
+        abi=VALIDATOR_SET_ABI_SIMPLIFIED, address=validator_set_contract_address
     )
 
     report_validator_call = validator_set_contract.functions.reportMaliciousValidator(
@@ -34,10 +49,3 @@ def report_malicious_validator(
     )
 
     print(f"Transaction hash: {encode_hex(transaction_receipt.transactionHash)}")
-
-
-def get_contract_abi(contract_name: str):
-    resource_package = __name__
-    json_string = pkg_resources.resource_string(resource_package, "contracts.json")
-    json_dict = json.loads(json_string)
-    return json_dict[contract_name]["abi"]
